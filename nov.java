55b28cc5ac3e2c9845a7cbd69865bcfd3fdd79e4
Rajesh Chandra Saha <55900566+RajeshSaha2001@users.noreply.github.com>
2020-10-16 18:35:16 +0530

    Add files via upload

---
+++ nov.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class b_charAt
+{
+
+    /* char charAt(int index)
+    Returns the char value at the specified index */
+    
+        System.out.println("Character="+s.charAt(2));
+    }
+}
