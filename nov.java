6753cc676f0a667039e809de3edd371ab59b1d07
deep629 <59963147+deep629@users.noreply.github.com>
2020-10-16 18:36:40 +0530

    solved the issue

--- nov.java
+++ nov.java
@@ -6,6 +6,13 @@ public class b_charAt
     /* char charAt(int index)
     Returns the char value at the specified index */
     
+    public static void main(String args[])
+    {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("enter a string");
+        s=sc.nextLine();
         System.out.println("Character="+s.charAt(2));
     }
 }
